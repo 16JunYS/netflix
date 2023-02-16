f10258add9f59ba5b2ede09678ba9c77a2c2ce17
16JunYS <popo3176junys@gmail.com>
2023-02-16 14:08:02 +0900

    테마 별 영화 리스트 조회 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import './App.css';
 import Banner from './components/Banner';
 import Nav from './components/Nav';
+import Row from "./components/Row";
+import requests from "./api/requests";
 
 function App() {
   return (
-    <div className="App">
+    <div className="app">
       <Nav/>
       <Banner/>
+
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      ></Row>
+      <Row
+        title="Trending Now"
+        id="TN"
+        fetchUrl={requests.fetchTrending}
+      ></Row>
+      <Row
+        title="Top Rated"
+        id="TR"
+        fetchUrl={requests.fetchTopRated}
+      ></Row>
     </div>
   );
 }
